63806f7875b3f666ee07f10de52bdd76e810ebe7
Nitishkumar Singh <nitishkumarsingh71@gmail.com>
2023-09-04 19:32:46 +0530

    include namespace support
    Signed-off-by: Nitishkumar Singh <nitishkumarsingh71@gmail.com>

--- client.go
+++ client.go
@@ -81,6 +81,221 @@ func (s *Client) GetNamespaces(ctx context.Context) ([]string, error) {
 	return namespaces, err
 }
 
+// GetNamespaces get openfaas namespaces
+func (s *Client) GetNamespace(ctx context.Context, namespace string) (types.FunctionNamespace, error) {
+	u := s.GatewayURL
+	u.Path = fmt.Sprintf("/system/namespace/%s", namespace)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return types.FunctionNamespace{}, fmt.Errorf("unable to create request for %s, error: %w", u.String(), err)
+	}
+
+	if s.ClientAuth != nil {
+		if err := s.ClientAuth.Set(req); err != nil {
+			return types.FunctionNamespace{}, fmt.Errorf("unable to set Authorization header: %w", err)
+		}
+	}
+
+	res, err := s.Client.Do(req)
+	if err != nil {
+		return types.FunctionNamespace{}, fmt.Errorf("unable to make HTTP request: %w", err)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	body, _ := io.ReadAll(res.Body)
+
+	switch res.StatusCode {
+	case http.StatusOK:
+		fnNamespace := types.FunctionNamespace{}
+		if err := json.Unmarshal(body, &fnNamespace); err != nil {
+			return types.FunctionNamespace{},
+				fmt.Errorf("unable to unmarshal value: %q, error: %w", string(body), err)
+		}
+		return fnNamespace, err
+
+	case http.StatusNotFound:
+		return types.FunctionNamespace{}, fmt.Errorf("namespace %s not found", namespace)
+
+	case http.StatusUnauthorized:
+		return types.FunctionNamespace{}, fmt.Errorf("unauthorized action, please setup authentication for this server")
+
+	default:
+		return types.FunctionNamespace{}, fmt.Errorf("unexpected status code: %d, message: %q", res.StatusCode, string(body))
+	}
+}
+
+// CreateNamespace creates a namespace
+func (s *Client) CreateNamespace(ctx context.Context, spec types.FunctionNamespace) (int, error) {
+
+	// set openfaas label
+	if spec.Labels == nil {
+		spec.Labels = map[string]string{}
+	}
+	spec.Labels["openfaas"] = "1"
+
+	bodyBytes, err := json.Marshal(spec)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
+	bodyReader := bytes.NewReader(bodyBytes)
+
+	u := s.GatewayURL
+	u.Path = "/system/namespace/"
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), bodyReader)
+	if err != nil {
+		return http.StatusBadGateway, err
+	}
+
+	if s.ClientAuth != nil {
+		if err := s.ClientAuth.Set(req); err != nil {
+			return http.StatusInternalServerError, fmt.Errorf("unable to set Authorization header: %w", err)
+		}
+	}
+
+	res, err := s.Client.Do(req)
+	if err != nil {
+		return http.StatusBadGateway, err
+	}
+
+	var body []byte
+	if res.Body != nil {
+		defer res.Body.Close()
+		body, _ = io.ReadAll(res.Body)
+	}
+
+	switch res.StatusCode {
+	case http.StatusAccepted, http.StatusOK, http.StatusCreated:
+		return res.StatusCode, nil
+
+	case http.StatusUnauthorized:
+		return res.StatusCode, fmt.Errorf("unauthorized action, please setup authentication for this server")
+
+	default:
+		return res.StatusCode, fmt.Errorf("unexpected status code: %d, message: %q", res.StatusCode, string(body))
+	}
+}
+
+// UpdateNamespace updates a namespace
+func (s *Client) UpdateNamespace(ctx context.Context, spec types.FunctionNamespace) (int, error) {
+
+	// set openfaas label
+	if spec.Labels == nil {
+		spec.Labels = map[string]string{}
+	}
+	spec.Labels["openfaas"] = "1"
+
+	bodyBytes, err := json.Marshal(spec)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
+	bodyReader := bytes.NewReader(bodyBytes)
+
+	u := s.GatewayURL
+	u.Path = fmt.Sprintf("/system/namespace/%s", spec.Name)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, u.String(), bodyReader)
+	if err != nil {
+		return http.StatusBadGateway, err
+	}
+
+	if s.ClientAuth != nil {
+		if err := s.ClientAuth.Set(req); err != nil {
+			return http.StatusInternalServerError, fmt.Errorf("unable to set Authorization header: %w", err)
+		}
+	}
+
+	res, err := s.Client.Do(req)
+	if err != nil {
+		return http.StatusBadGateway, err
+	}
+
+	var body []byte
+	if res.Body != nil {
+		defer res.Body.Close()
+		body, _ = io.ReadAll(res.Body)
+	}
+
+	switch res.StatusCode {
+	case http.StatusAccepted, http.StatusOK, http.StatusCreated:
+		return res.StatusCode, nil
+
+	case http.StatusNotFound:
+		return res.StatusCode, fmt.Errorf("namespace %s not found", spec.Name)
+
+	case http.StatusUnauthorized:
+		return res.StatusCode, fmt.Errorf("unauthorized action, please setup authentication for this server")
+
+	default:
+		return res.StatusCode, fmt.Errorf("unexpected status code: %d, message: %q", res.StatusCode, string(body))
+	}
+}
+
+// DeleteNamespace deletes a namespace
+func (s *Client) DeleteNamespace(ctx context.Context, namespace string) error {
+
+	delReq := types.FunctionNamespace{
+		Name: namespace,
+		Labels: map[string]string{
+			"openfaas": "1",
+		},
+	}
+
+	var err error
+
+	bodyBytes, _ := json.Marshal(delReq)
+	bodyReader := bytes.NewReader(bodyBytes)
+
+	u := s.GatewayURL
+	u.Path = fmt.Sprintf("/system/namespace/%s", namespace)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, u.String(), bodyReader)
+	if err != nil {
+		return fmt.Errorf("cannot connect to OpenFaaS on URL: %s, error: %s", u.String(), err)
+	}
+
+	if s.ClientAuth != nil {
+		if err := s.ClientAuth.Set(req); err != nil {
+			return fmt.Errorf("unable to set Authorization header: %w", err)
+		}
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("cannot connect to OpenFaaS on URL: %s, error: %s", s.GatewayURL, err)
+
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	switch res.StatusCode {
+	case http.StatusAccepted, http.StatusOK, http.StatusCreated:
+		break
+
+	case http.StatusNotFound:
+		return fmt.Errorf("namespace %s not found", namespace)
+
+	case http.StatusUnauthorized:
+		return fmt.Errorf("unauthorized action, please setup authentication for this server")
+
+	default:
+		var err error
+		bytesOut, err := io.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+
+		return fmt.Errorf("unexpected status code: %d, message: %q", res.StatusCode, string(bytesOut))
+	}
+	return nil
+}
+
 // GetFunctions lists all functions
 func (s *Client) GetFunctions(ctx context.Context, namespace string) ([]types.FunctionStatus, error) {
 	u := s.GatewayURL
