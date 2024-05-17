5303651905bbb252332e939f241599056d196752
Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>
2024-05-17 17:28:01 +0200

    Have TokenAuth type implement the TokenSource interface
    Signed-off-by: Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>

--- iam_auth.go
+++ iam_auth.go
@@ -32,24 +32,37 @@ type TokenAuth struct {
 // Set validates the token expiry on each call. If it's expired it will exchange
 // an ID token from the TokenSource for a new OpenFaaS token.
 func (a *TokenAuth) Set(req *http.Request) error {
+	token, err := a.getToken()
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+token)
+	return nil
+}
+
+func (a *TokenAuth) Token() (string, error) {
+	return a.getToken()
+}
+
+func (a *TokenAuth) getToken() (string, error) {
 	a.lock.Lock()
 	defer a.lock.Unlock()
 
 	if a.token == nil || a.token.Expired() {
 		idToken, err := a.TokenSource.Token()
 		if err != nil {
-			return err
+			return "", err
 		}
 
 		token, err := ExchangeIDToken(a.TokenURL, idToken)
 		if err != nil {
-			return err
+			return "", err
 		}
 		a.token = token
 	}
 
-	req.Header.Add("Authorization", "Bearer "+a.token.IDToken)
-	return nil
+	return a.token.IDToken, nil
 }
 
 // A TokenSource to get ID token by reading a Kubernetes projected service account token
