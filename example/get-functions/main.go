c947b049956b5a1bfda78020d24af7275b877ffc
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2025-08-04 16:24:58 +0100

    Improve get-functions example
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- example/get-functions/main.go
+++ example/get-functions/main.go
@@ -2,21 +2,46 @@ package main
 
 import (
 	"context"
+	b64 "encoding/base64"
+	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
-	"github.com/openfaas/faas-provider/types"
+	execute "github.com/alexellis/go-execute/pkg/v2"
 	"github.com/openfaas/go-sdk"
 )
 
-func main() {
-	// NOTE: You can have any name for environment variables. below defined variables names are not standard names
-	username := os.Getenv("OPENFAAS_USERNAME")
-	password := os.Getenv("OPENFAAS_PASSWORD")
+// main will:
 
-	gatewayURL, _ := url.Parse(os.Getenv("OPENFAAS_GATEWAY_URL"))
+// Get all functions, and print the number found.
+// Iterate each, and print the various details available.
+func main() {
+	var (
+		gateway,
+		username,
+		password string
+	)
+
+	flag.StringVar(&gateway, "gateway", "http://127.0.0.1:8080", "The URL of the OpenFaaS gateway")
+	flag.StringVar(&username, "username", "admin", "The username to use for authentication")
+	flag.StringVar(&password, "password", "", "The password to use for authentication")
+
+	flag.Parse()
+
+	if len(password) == 0 {
+		fmt.Println("No --password provided, looking up via kubectl")
+		password = lookupPasswordViaKubectl()
+	}
+
+	gatewayURL, err := url.Parse(gateway)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Invalid gateway URL: %s", err)
+		os.Exit(1)
+	}
+
 	auth := &sdk.BasicAuth{
 		Username: username,
 		Password: password,
@@ -24,39 +49,92 @@ func main() {
 
 	client := sdk.NewClient(gatewayURL, auth, http.DefaultClient)
 
-	fns, err := client.GetFunctions(context.Background(), "openfaas-fn")
+	functions, err := client.GetFunctions(context.Background(), "openfaas-fn")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Get Failed: %s", err)
 		os.Exit(1)
 	}
-	fmt.Printf("No Of Functions: %d\n", len(fns))
+	fmt.Printf("Found %d functions\n", len(functions))
+	fmt.Println("--------------------------------")
 
-	status, err := client.Deploy(context.Background(), types.FunctionDeployment{
-		Service:    "env-store-test",
-		Image:      "ghcr.io/openfaas/alpine:latest",
-		Namespace:  "openfaas-fn",
-		EnvProcess: "env",
-		Labels: &map[string]string{
-			"purpose": "test",
-		},
-	})
-	// non 200 status value will have some error
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Status: %d Deploy Failed: %s", status, err)
-		os.Exit(1)
+	for _, function := range functions {
+		fmt.Printf("Function: %s\n", function.Name)
+		fmt.Printf("Image: %s\n", function.Image)
+		if function.Labels != nil {
+			fmt.Printf("Labels:\n")
+			for key, value := range *function.Labels {
+				fmt.Printf("  %s: %s\n", key, value)
+			}
+		} else {
+			fmt.Printf("Labels: <none>\n")
+		}
+		if function.Annotations != nil {
+			fmt.Printf("Annotations:\n")
+			for key, value := range *function.Annotations {
+				fmt.Printf("  %s: %s\n", key, value)
+			}
+		} else {
+			fmt.Printf("Annotations: <none>\n")
+		}
+
+		if function.EnvVars != nil {
+			fmt.Printf("EnvVars:\n")
+			for key, value := range function.EnvVars {
+				fmt.Printf("  %s: %s\n", key, value)
+			}
+		} else {
+			fmt.Printf("EnvVars: <none>\n")
+		}
+
+		if function.Secrets != nil {
+			fmt.Printf("Secrets:\n")
+			for _, value := range function.Secrets {
+				fmt.Printf("  -%s\n", value)
+			}
+		} else {
+			fmt.Printf("Secrets: <none>\n")
+		}
+
+		if function.Requests != nil {
+			fmt.Printf("Requests: %+v\n", function.Requests)
+		} else {
+			fmt.Printf("Requests: <none>\n")
+		}
+
+		if function.Limits != nil {
+			fmt.Printf("Limits: %+v\n", function.Limits)
+		} else {
+			fmt.Printf("Limits: <none>\n")
+		}
+		fmt.Println("--------------------------------")
+	}
+}
+
+func lookupPasswordViaKubectl() string {
+
+	cmd := execute.ExecTask{
+		Command:      "kubectl",
+		Args:         []string{"get", "secret", "-n", "openfaas", "basic-auth", "-o", "jsonpath='{.data.basic-auth-password}'"},
+		StreamStdio:  false,
+		PrintCommand: false,
 	}
 
-	fns, err = client.GetFunctions(context.Background(), "openfaas-fn")
+	res, err := cmd.Execute(context.Background())
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Get Failed: %s", err)
-		os.Exit(1)
+		panic(err)
 	}
-	fmt.Printf("No Of Functions: %d\n", len(fns))
 
-	err = client.DeleteFunction(context.Background(), "env-store-test", "openfaas-fn")
-	// non 200 status value will have some error
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Delete Failed: %s", err)
-		os.Exit(1)
+	if res.ExitCode != 0 {
+		panic("Non-zero exit code: " + res.Stderr)
 	}
+	resOut := strings.Trim(res.Stdout, "\\'")
+
+	decoded, err := b64.StdEncoding.DecodeString(resOut)
+	if err != nil {
+		panic(err)
+	}
+
+	password := strings.TrimSpace(string(decoded))
+
+	return password
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,12 @@
 module github.com/openfaas/go-sdk
 
-go 1.23
+go 1.24
 
-require github.com/openfaas/faas-provider v0.25.5
+require github.com/openfaas/faas-provider v0.25.8
 
-require github.com/google/go-cmp v0.6.0
+require github.com/google/go-cmp v0.7.0
 
-require github.com/alexellis/hmac/v2 v2.0.0
+require (
+	github.com/alexellis/go-execute v0.6.0
+	github.com/alexellis/hmac/v2 v2.0.0
+)
